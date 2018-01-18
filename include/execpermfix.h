ea11feef0f8f01c825be9bc9a2aaf036446d62e4
Leandro Lisboa Penz <lpenz@lpenz.org>
2018-01-18 23:22:48 +0000

    exerpermfix: fix ELF detection; minor refactoring
    - mmap the file before reading it
    - use elf.h defines and structs
    - handle endianness

--- include/execpermfix.h
+++ include/execpermfix.h
@@ -8,16 +8,17 @@
 #ifndef EXECPERMFIX_H_INCLUDED
 #define EXECPERMFIX_H_INCLUDED
 
+#include <stdbool.h>
 #include <sys/stat.h>
 
 /**
  * \brief  Check if a file should BE executable by its contents.
- * \param  name Path of entity to process.
+ * \param  filename Path of file to process.
  * \param  fd File descriptor of entity (result of open).
  * \param  st Stat structure of entity (result of stat).
  * \return 0 if not executable, 1 if executable, -1 if error.
  */
-int fileIsExec(const char *name, int fd, const struct stat *st);
+int fileIsExec(const char *filename, int fd, const struct stat *st);
 
 /**
  * \brief  Processes a file or a directory recursively.
@@ -26,6 +27,6 @@ int fileIsExec(const char *name, int fd, const struct stat *st);
  * \param  verbose 1 to print actions.
  * \return 0 if ok, 1 if error.
  */
-int execpermfix(const char *name, int noop, int verbose);
+int execpermfix(const char *name, bool noop, bool verbose);
 
 #endif /* EXECPERMFIX_H_INCLUDED */
--- src/execpermfix.c
+++ src/execpermfix.c
@@ -5,13 +5,20 @@
  */
 /****************************************************************************/
 
+#define _XOPEN_SOURCE 700  // swab
+#include <arpa/inet.h>
+#include <assert.h>
 #include <dirent.h>
+#include <elf.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -19,67 +26,117 @@
 
 /****************************************************************************/
 
-int fileIsExec(const char *name, int fd, const struct stat *st) {
+static int filemapIsScript(const void *addr, size_t size) {
     const char scriptMagic[] = "#!";
-    struct {
-        uint8_t ei_magic[4];
-        uint8_t ei_class;
-        uint8_t ei_data;
-        uint8_t ei_version;
-        uint8_t ei_pad[9];
-        uint16_t e_type;
-    } * eh;
-    const char elfMagic[] = {0x7f, 0x45, 0x4c, 0x46};
-    char buf[sizeof(*eh)];
-    int r;
-    int s;
 
-    s = st->st_size > (signed)sizeof(*eh) ? (signed)sizeof(*eh) : st->st_size;
-
-    if (s < 2) return 0;
-
-    if ((r = read(fd, buf, s)) < 0) {
-        fprintf(stderr, "%s: error in read\n", name);
-        return -1;
-    }
-
-    if (r >= (signed)(sizeof scriptMagic - 1) &&
-        memcmp(buf, scriptMagic, sizeof scriptMagic - 1) == 0) {
-        return 1;
-    }
-
-    eh = (typeof(eh))buf;
-    if (r >= (signed)sizeof(*eh) &&
-        memcmp(eh->ei_magic, elfMagic, sizeof elfMagic) == 0 &&
-        ((eh->ei_data == 2 && eh->e_type == 0x0200) ||
-         (eh->ei_data == 1 && eh->e_type == 0x0002))) {
+    if (size >= (signed)(sizeof scriptMagic - 1) &&
+        memcmp(addr, scriptMagic, sizeof scriptMagic - 1) == 0) {
         return 1;
     }
 
     return 0;
 }
 
+static int filemapIsElf(const char *filename, const void *addr, size_t size) {
+    if (size < EI_NIDENT) return 0;
+
+    if (memcmp(addr, ELFMAG, SELFMAG) != 0) {
+        return 0;
+    }
+
+    const uint8_t *elfclass = (uint8_t *)addr + EI_CLASS;
+    const uint8_t *elfdata = (uint8_t *)addr + EI_DATA;
+    int e_type_norm = 0;
+
+    if (*elfclass == ELFCLASS32) {
+        const Elf32_Ehdr *hdr = addr;
+        Elf32_Half e_type;
+        assert(sizeof e_type == sizeof hdr->e_type);
+        if (*elfdata == ELFDATA2LSB) {
+            e_type = hdr->e_type;
+        } else if (*elfdata == ELFDATA2MSB) {
+            swab(&hdr->e_type, &e_type, sizeof e_type);
+        } else {
+            fprintf(stderr, "%s: unknown ELF data %d\n", filename, *elfdata);
+            return -1;
+        }
+        e_type_norm = e_type;
+    } else if (*elfclass == ELFCLASS64) {
+        const Elf64_Ehdr *hdr = addr;
+        Elf64_Half e_type;
+        assert(sizeof e_type == sizeof hdr->e_type);
+        if (*elfdata == ELFDATA2LSB) {
+            e_type = hdr->e_type;
+        } else if (*elfdata == ELFDATA2MSB) {
+            swab(&hdr->e_type, &e_type, sizeof e_type);
+        } else {
+            fprintf(stderr, "%s: unknown ELF data %d\n", filename, *elfdata);
+            return -1;
+        }
+        e_type_norm = e_type;
+    } else {
+        fprintf(stderr, "%s: unknown ELF class %d\n", filename, *elfclass);
+        return -1;
+    }
+
+    if (e_type_norm == ET_EXEC || e_type_norm == ET_DYN) {
+        return 1;
+    } else if (e_type_norm == ET_REL || e_type_norm == ET_CORE ||
+               e_type_norm == ET_LOPROC || e_type_norm == ET_HIPROC) {
+        return 0;
+    } else {
+        fprintf(stderr, "%s: unknown ELF type %d\n", filename, e_type_norm);
+        return -1;
+    }
+
+    return -1;
+}
+
+int fileIsExec(const char *filename, int fd, const struct stat *st) {
+    size_t size = st->st_size;
+    void *addr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    if (addr == MAP_FAILED) {
+        fprintf(stderr, "%s: mmap error %s\n", filename, strerror(errno));
+        return -1;
+    }
+
+    int ret = filemapIsScript(addr, size);
+    if (ret == 0) {
+        ret = filemapIsElf(filename, addr, size);
+    }
+
+    if (munmap(addr, size) != 0) {
+        fprintf(stderr, "%s: munmap error %s, keep going\n", filename,
+                strerror(errno));
+    }
+
+    return ret;
+}
+
 /****************************************************************************/
 
-int execpermfix(const char *name, int noop, int verbose) {
+int execpermfix(const char *name, int noop, bool verbose) {
     int fd = 0;
     struct stat st;
-    char fullname[PATH_MAX + 1];
+    char *fullname;
     int exec;
     mode_t mode;
 
-    if (realpath(name, fullname) == NULL) {
+    if ((fullname = realpath(name, NULL)) == NULL) {
         fprintf(stderr, "%s: realpath error %s\n", name, strerror(errno));
         return 1;
     }
 
     if ((fd = open(fullname, O_RDONLY)) < 0) {
         fprintf(stderr, "%s: open error %s\n", fullname, strerror(errno));
+        free(fullname);
         return 1;
     }
 
     if (fstat(fd, &st) != 0) {
         fprintf(stderr, "%s: fstat error %s\n", fullname, strerror(errno));
+        free(fullname);
         close(fd);
         return 1;
     }
@@ -87,6 +144,7 @@ int execpermfix(const char *name, int noop, int verbose) {
     if (!S_ISDIR(st.st_mode) && !S_ISREG(st.st_mode)) {
         if (verbose)
             printf("%s skipped, neither a file nor a directory\n", fullname);
+        free(fullname);
         close(fd);
         return 0;
     }
@@ -98,6 +156,7 @@ int execpermfix(const char *name, int noop, int verbose) {
     else if (S_ISREG(st.st_mode))
         exec = fileIsExec(fullname, fd, &st);
     if (exec < 0) {
+        free(fullname);
         close(fd);
         return -1;
     }
@@ -113,6 +172,7 @@ int execpermfix(const char *name, int noop, int verbose) {
 
     if (mode == st.st_mode) {
         if (verbose) printf("%s: %o would not be changed\n", name, st.st_mode);
+        free(fullname);
         close(fd);
         return 0;
     }
@@ -121,16 +181,19 @@ int execpermfix(const char *name, int noop, int verbose) {
     if (noop) {
         if (verbose)
             printf("%s: %o would change to %o\n", name, st.st_mode, mode);
+        free(fullname);
         close(fd);
         return 0;
     }
     if (fchmod(fd, mode) < 0) {
         fprintf(stderr, "%s: chmod error %s\n", name, strerror(errno));
+        free(fullname);
         close(fd);
         return 1;
     }
     if (verbose) printf("%s: %o changed to %o\n", name, st.st_mode, mode);
 
+    free(fullname);
     close(fd);
 
     return 0;

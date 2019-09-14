129133fcbca84d4e4906c6b7e15db3d155572b15
Leandro Lisboa Penz <lpenz@lpenz.org>
2019-09-14 19:37:51 +0100

    CI: use lpenz/ghaction-cmake instead of local build actions

--- .github/actions/clang-analyzers/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
-
-find "$PWD" \( -name '*.[ch]' -o -name '*.cc' \) -exec "$INPUT_ANALYZER" -p "$PWD" {} +
--- .github/actions/clang-format/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-find "$PWD" \( -name '*.[ch]' -o -name '*.cc' \) -exec clang-format -i {} +
-

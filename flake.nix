f33b88adf4dd6efaa9d9fa262d436c14ddaf381e
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-06-18 16:55:32 +0100

    flake: use nix 23.05

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   description = "Fixes executable permissions";
+
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages."${system}";
       in
       rec {
         packages.default = pkgs.stdenv.mkDerivation {

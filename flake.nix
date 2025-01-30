dc00adae307888ec09f02e2260b3c67ae9b4f610
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-01-30 21:24:47 +0000

    flake.*: use nix 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fixes executable permissions";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
